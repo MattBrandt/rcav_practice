e3217b0ed718566a6feee6b762915cfdcef5467e
matt brandt <mattbrandt87@gmail.com>
2014-05-06 22:26:12 -0500

    PMT Latest Attempt

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -16,5 +16,13 @@ end
 
 
   def pmt
+    @the_interest_rate = params[:interest_rate].to_f
+    @the_number_of_payments = params[:number_of_payments].to_f
+    @the_principle_value = params[:principal_value].to_f
+    # @payment = @the_number_of_payments * @the_principle_value
+
+    @payment = @the_principal_value * @the_number_of_payments * @the_interest_rate
+
   end
 end
+# interest_rate/:number_of_payments/:present_value
